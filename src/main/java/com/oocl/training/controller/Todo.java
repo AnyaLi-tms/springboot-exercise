875dda7a5937290c2118d538f5cc5e61e0ac4486
LIAN18 <lian18@oocl.com>
2025-08-26 17:37:10 +0800

    init

---
+++ src/main/java/com/oocl/training/controller/Todo.java
@@ -0,0 +1,34 @@
+package com.oocl.training.controller;
+
+public class Todo {
+    private int id;
+    private String title;
+    private String status;
+
+    public Todo() {
+
+    }
+
+    public Todo(int id, String title, String status) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int setId(int id) {
+        this.id = id;
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
