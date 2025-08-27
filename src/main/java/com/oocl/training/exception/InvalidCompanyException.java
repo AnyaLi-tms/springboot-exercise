cd32f32d8e322f417fd93d02d6114232b899a0f1
LIAN18 <lian18@oocl.com>
2025-08-27 13:26:09 +0800

    feat(company): split to three layers

---
+++ src/main/java/com/oocl/training/exception/InvalidCompanyException.java
@@ -0,0 +1,7 @@
+package com.oocl.training.exception;
+
+public class InvalidCompanyException extends RuntimeException {
+    public InvalidCompanyException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/oocl/training/repository/CompanyRepository.java
@@ -0,0 +1,4 @@
+package com.oocl.training.repository;
+
+public class CompanyRepository {
+}
